c9b4fe8ed71ef49c63a271dfa2e6f70ffc43e8cc
ely966 <46519177+ely966@users.noreply.github.com>
2022-03-06 18:51:13 +0100

    a

--- src/main/java/com/example/demo/ProyectoApiVeterinaria2Application.java
+++ src/main/java/com/example/demo/ProyectoApiVeterinaria2Application.java
@@ -3,9 +3,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProyectoApiVeterinaria2Application {
+public class ProyectoApiVeterinaria2Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApiVeterinaria2Application.class, args);

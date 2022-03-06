95913e6033e1023ed82f77c2b176779b3fd16465
ely966 <46519177+ely966@users.noreply.github.com>
2022-03-06 17:54:43 +0100

    añadiendo prof

--- src/main/java/com/example/demo/configuration/CorsConfiguration.java
+++ src/main/java/com/example/demo/configuration/CorsConfiguration.java
@@ -16,7 +16,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("**")
-				.allowedOrigins("http://localhost:4200")
+				.allowedOrigins("https://ely966.github.io/proyectoVeterinariaF/:*")
 				.allowedHeaders("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD","HEADER")
 				.allowedMethods("GET", "POST", "PUT", "DELETE");
 				//maxAge(3600)

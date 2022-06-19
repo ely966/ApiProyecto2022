a10544ff8b1691c0c0e5b364cba7abe3b0eaa63e
ely966 <46519177+ely966@users.noreply.github.com>
2022-06-19 17:00:59 +0200

    intentar comunicacion de api con front

--- src/main/java/com/example/demo/configuration/CorsConfiguration.java
+++ src/main/java/com/example/demo/configuration/CorsConfiguration.java
@@ -16,7 +16,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("**")
-				.allowedOrigins("https://ely966.github.io")
+				.allowedOrigins("https://ely966.github.io/")
 				.allowedHeaders("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD","HEADER")
 				.allowedMethods("GET", "POST", "PUT", "DELETE");
 				//maxAge(3600)//https://ely966.github.io
